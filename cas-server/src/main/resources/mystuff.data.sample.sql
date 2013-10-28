4ca29a52dd270e72b264b1cc4e678365363954e6
Cael <adolfoecs@gmail.com>
2013-10-28 18:14:02 +0000

    [u] Update on the setup of Cas Server and My Stuff Server to share database and also entities.

---
+++ cas-server/src/main/resources/mystuff.data.sample.sql
@@ -0,0 +1,25 @@
+INSERT INTO `mystuff`.`USER`
+(`id`,
+`email`,
+`password`,
+`cellPhone`,
+`username`)
+VALUES
+(1,
+"test@test.com",
+"test",
+"0123456789",
+"test");
+
+INSERT INTO `mystuff`.`USER`
+(`id`,
+`email`,
+`password`,
+`cellPhone`,
+`username`)
+VALUES
+(2,
+"admin@test.com",
+"admin",
+"0123456789",
+"admin");
