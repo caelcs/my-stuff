325c659abee046e977ac8a320f2fb33e59c5a73d
Cael <adolfoecs@gmail.com>
2013-11-15 13:49:30 +0000

    #8577049226813 - Upgrade Servlet API to 3.1.0 #8553336778929 - Upgrade Jersey to 2.4.1 and modified the way to load the application using a Spring Extension.

--- my-stuff-server/src/main/java/com/caeldev/MyStuffApplication.java
+++ my-stuff-server/src/main/java/com/caeldev/MyStuffApplication.java
@@ -1,29 +1,35 @@
-package com.caeldev.services
+package com.caeldev;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
 
 /**
  * Copyright (c) 2012 - 2013 Caeldev, Inc.
- *
+ * <p/>
  * User: cael
- * Date: 26/10/2013
- * Time: 18:12
- *
+ * Date: 15/11/2013
+ * Time: 11:02
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
-object Operation {
-  case class List(pageQuery:PageQuery)
-  case class Add(entity:Any)
-  case class Delete(id:Long)
-  case class Update(entity:Any)
-  case class Get(id:Long)
+public class MyStuffApplication extends ResourceConfig {
+
+    public MyStuffApplication () {
+        register(RequestContextFilter.class);
+        register(JacksonFeature.class);
+        packages("com.caeldev.resources"
+                , "com.caeldev.providers");
+    }
+
 }
